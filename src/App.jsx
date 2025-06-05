b3e8759761b6cdbc74e5b40d56d425c42d8be505
오지환 <ovitwisdom@gmail.com>
2025-06-06 04:23:28 +0900

    초기 세팅 완료

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<div className="intro-area">
+				<div class="test">
+					dsa
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@public': path.resolve(__dirname, 'public'),
+			'@img': path.resolve(__dirname, 'src/assets/imgs/img'),
+			'@ico': path.resolve(__dirname, 'src/assets/imgs/ico'),
+			'@scss': path.resolve(__dirname, 'src/assets/scss'),
+		},
+	}
+})
