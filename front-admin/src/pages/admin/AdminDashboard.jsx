c0719026853e42f9e2f4161d2c1f10805ceddac5
이민우 <minwoo552@gmail.com>
2025-06-25 10:34:37 +0900

    관리자수정

--- front-admin/src/pages/admin/AdminDashboard.jsx
+++ front-admin/src/pages/admin/AdminDashboard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../../assets/scss/AdminDashboard.scss';
-import logo from '../../assets/imgs/ico/logo.svg';
+import logo from '../../assets/imgs/ico/logoc.svg';
 
 export default function AdminDashboard() {
   return (
     <div className="dashboard-admin" style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '70vh'}}>
-      <img src={logo} alt="로고" style={{width: 180, marginBottom: 32}} />
+      <img src={logo} alt="로고" style={{width: 300, marginBottom: -64, opacity: 0.5}} />
     </div>
   );
 }
