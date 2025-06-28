c96cc12ace34554dd29bcf574715dc8e4a46859d
노현지 <shguswl826@gmail.com>
2025-06-29 05:28:37 +0900

    config 중복 선언 변경

--- front-server/vite.config.js
+++ front-server/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: 'true',
-    allowedHosts: ['thebridgehub.org'],
+    host: '0.0.0.0', // 올바른 설정: 외부 접속 허용
     port: 7000,
+    allowedHosts: ['thebridgehub.org'],
   },
   resolve: {
     alias: {
@@ -21,27 +20,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "src/page/components"),
       "@common": path.resolve(__dirname, "src/page/common"),
       "@json": path.resolve(__dirname, "src/json"),
-      "@dev": path.resolve(__dirname, "src/dev")
+      "@dev": path.resolve(__dirname, "dev"),
     },
   },
-	plugins: [react()],
-	server: {
-    host: 'true',
-    allowedHosts: ['thebridgehub.org'],
-		port: 7000
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src'),
-			'@img': path.resolve(__dirname, 'src/assets/imgs/img'),
-			'@ico': path.resolve(__dirname, 'src/assets/imgs/ico'),
-			'@scss': path.resolve(__dirname, 'src/assets/scss'),
-			'@js': path.resolve(__dirname, 'src/assets/js'),
-			'@page': path.resolve(__dirname, 'src/page'),
-			'@components': path.resolve(__dirname, 'src/page/components'),
-			'@common': path.resolve(__dirname, 'src/page/common'),
-			'@json': path.resolve(__dirname, 'src/json'),
-			'@dev': path.resolve(__dirname, 'dev'),
-		},
-	}
-})
+});
