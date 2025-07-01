ab3574e1c3a523fcb0a66e979b8fe1f3bc676e60
오지환 <ovitwisdom@gmail.com>
2025-07-01 23:42:58 +0900

    liberty

--- rtc-server/src/util/mysql.mjs
+++ rtc-server/src/util/mysql.mjs
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'liberty',
+  password: '1234',
   database: 'thebridgehub',
   waitForConnections: true,
   connectionLimit: 10,
